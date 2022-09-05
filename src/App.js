78e86b723e1f06b19e8bbb6e1191d7f2d2f64de2
RameshSuthar <rameshsuthar1231@gmail.com>
2022-09-06 01:57:08 +0530

    Added 404 page for not found route.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Routes, Route, Link } from "react-router-dom";
 import Login from './components/Login/Login';
 import PrivateRoute from './components/ProtectedRoute';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
+        <Route path='*' element={<NotFound />} />
         <Route path="/" exact element={<PrivateRoute><Home /></PrivateRoute>} />
         <Route path="/login" exact element={<Login />} />
       </Routes>
